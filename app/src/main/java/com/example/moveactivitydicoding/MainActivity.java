43ca7078b8b9dbe529cffa1d246d9ddbfc908084
indahratnasari <indahrs05@gmail.com>
2019-09-13 21:21:00 +0700

    penambahan Intent Implicit dial number

--- app/src/main/java/com/example/moveactivitydicoding/MainActivity.java
+++ app/src/main/java/com/example/moveactivitydicoding/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.moveactivitydicoding;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -13,11 +14,17 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ///menginisialisasi button yang sudah ditambahkan
         Button btnMoveActivity = findViewById(R.id.btn_move_activity);
         btnMoveActivity.setOnClickListener(this);
 
+        ///menginisialisasi button yang sudah ditambahkan
         Button btnMoveWithDataActivity = findViewById(R.id.btn_move_activity_data);
         btnMoveWithDataActivity.setOnClickListener(this);
+
+        ///menginisialisasi button yang sudah ditambahkan
+        Button btnDialPhone = findViewById(R.id.btn_dial_number);
+        btnDialPhone.setOnClickListener(this);
     }
 
     @Override
@@ -27,12 +34,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 Intent moveIntent = new Intent(MainActivity.this, MoveActivty.class);
                 startActivity(moveIntent);
                 break;
+
             case R.id.btn_move_activity_data:
                 Intent moveWithDataIntent = new Intent(MainActivity.this, MoveWithDataActivity.class);
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_NAME, "DicodingAcademy Boy");
                 moveWithDataIntent.putExtra(MoveWithDataActivity.EXTRA_AGE, 5);
                 startActivity(moveWithDataIntent);
                 break;
+
+                //dengan melakukan casting tombol Dial Number, set listener dan menambahkan action ketika tombol diklik
+            case R.id.btn_dial_number:
+                String phoneNumber = "081336736552";
+                Intent dialPhoneIntent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:"+phoneNumber));
+                startActivity(dialPhoneIntent);
+                break;
         }
     }
+
 }
